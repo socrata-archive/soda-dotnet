9f7740337e9d3b856e347747f4937819a09282fa
Dan Rathbone <dan.rathbone@gmail.com>
2013-08-06 12:46:16 -0700

    adding blank integration test, experimenting with test playlist

---
+++ Test/integration/DatasetIntegrationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.integration
+{
+    [TestClass]
+    public class DatasetIntegrationTest
+    {
+        [TestMethod]
+        public void NoopIntegrationTest()
+        {
+        }
+    }
+}
